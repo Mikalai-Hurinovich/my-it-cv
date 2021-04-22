30721a7f677d35095eb0f72c7fd0f7cbc665d43d
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-04-22 21:59:55 +0300

    add: telegram & gitHub icons

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import s from './Contacts.module.css'
+import github from './../../assets/img/github.png'
+import telegram from './../../assets/img/telegram.png'
 
 const Contacts = () => {
     return (
@@ -9,8 +11,14 @@ const Contacts = () => {
                 <input className={s.input} type="text"/>
                 <input className={s.input} type="text"/>
                 <textarea className={s.textarea} placeholder={'Some text here...'}></textarea>
-                <a href="/">GitHub</a>
-                <a href="/">Telegram</a>
+                <div className={s.form__contacts}>
+                    <a target="_blank" href="https://github.com/Mikalai-Hurinovich">
+                        <img src={github} alt="githubImg"/>
+                    </a>
+                    <a target="_blank" href="https://t.me/NikolaTeslavich">
+                        <img src={telegram} alt="telegramImg"/>
+                    </a>
+                </div>
             </div>
             <button className={s.send}>Send</button>
         </div>
