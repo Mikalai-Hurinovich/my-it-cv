00df1347b8d8510f4fddef587af97e3028d65523
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-05-14 10:10:53 +0300

    feat: add single button module

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './Project.module.scss'
+import Button from "../../../common/Components/Button/Button";
 
 const Project = (props) => {
     return (
         <div className={s.project__card}>
             <div className={s.project__img} style={props.style}>
-                <button className={s.project__card_button}>Show</button>
+                <Button title={'Show'}/>
+                {/*<button className={s.project__card_button}>Show</button>*/}
             </div>
             <h3 className={s.project__title}>{props.title}</h3>
             <div className={s.project__description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad beatae
