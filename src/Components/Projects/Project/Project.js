82f063ed57896d581b01764a177e6b8a183ba374
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-06-08 08:21:16 +0300

    feat: react reveal

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import s from './Project.module.scss'
 import Button from "../../../common/Components/Button/Button";
+import Fade from "react-reveal/Fade";
 
 const Project = (props) => {
     return (
-        <div className={s.projectCard}>
-            <div className={s.projectImg} style={props.style}>
-                <Button className={s.projectButton} title={'Show'}/>
-            </div>
-            <h3 className={s.projectTitle}>{props.title}</h3>
-            <div className={s.projectTechnologies}>Technologies: {props.technologies}</div>
-            <div className={s.projectDescription}>{props.description}
-            </div>
+        <Fade>
+            <div className={s.projectCard}>
+                <div className={s.projectImg} style={props.style}>
+                    <Button className={s.projectButton} title={'Show'}/>
+                </div>
+                <h3 className={s.projectTitle}>{props.title}</h3>
+                <div className={s.projectTechnologies}>Technologies: {props.technologies}</div>
+                <div className={s.projectDescription}>{props.description}
+                </div>
 
-        </div>
+            </div>
+        </Fade>
     );
 };
 
